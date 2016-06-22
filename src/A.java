76c20315d1408a92cb5659e8c95712ad5d1999cc
larinme <maksim_larin@yahoo.com>
2016-06-23 00:42:56 +0300

    cycg

--- src/A.java
+++ src/A.java
@@ -6,5 +6,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("Hello, Max!!");
         System.out.println("Hello, Max!!");
+        System.out.println("Hello, Max!!");
+        System.out.println("Hello, Max!!");
     }
 }
